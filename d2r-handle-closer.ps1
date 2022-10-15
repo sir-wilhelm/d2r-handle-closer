1918aa1bc50f8d2ccf6e9439eedcb856e28317f9
sir-wilhelm <sir-wilhelm@users.noreply.github.com>
2022-10-15 12:35:52 -0500

    Force handle to be in the same path as script.

--- d2r-handle-closer.ps1
+++ d2r-handle-closer.ps1
@@ -1,24 +1,19 @@
 #Requires -RunAsAdministrator
 
-$handleInPath = get-command handle -ErrorAction Ignore
-$handleApp = get-item handle.exe -ErrorAction Ignore
-if (!$handleInPath -and !$handleApp) {
-    Write-Host "handle.exe not found, make sure it's in the PATH or script folder."
+if (!(Test-Path handle.exe)) {
+    Write-Host "handle.exe not found, make sure it's in script folder."
     exit 1
-}
-
-if ($handleApp) {
-    $env:Path += ";$($handleApp.Directory)"
 }
 
 do {
 
-$handle = handle -a -p d2 "Check For Other Instances" -nobanner | Out-String
-if ($handle -match "pid:\s+(?<d2pid>\d+)\s+type:\s+Event\s+(?<eventHandle>\w+):") {
-    $d2pid = $matches["d2pid"]
-    $eventHandle = $matches["eventHandle"]
-    handle -c $eventHandle -p $d2pid -y -nobanner
-}
+    $handle = .\handle.exe -a -p d2 "Check For Other Instances" -nobanner | Out-String
+    if ($handle -match "pid:\s+(?<d2pid>\d+)\s+type:\s+Event\s+(?<eventHandle>\w+):") {
+        $d2pid = $matches["d2pid"]
+        $eventHandle = $matches["eventHandle"]
+        handle -c $eventHandle -p $d2pid -y -nobanner
+    }
+    
+    Start-Sleep 10
 
-Start-Sleep 10
 } while (1)
