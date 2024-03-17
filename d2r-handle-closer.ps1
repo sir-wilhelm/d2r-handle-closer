c1a63d8aced3cc818ca3e76ec6c15aa7f4c756b6
sir-wilhelm <sir-wilhelm@users.noreply.github.com>
2024-03-17 17:12:16 -0500

    Support having handle available in the system path.
    This also allows you to run the script from any directory.

--- d2r-handle-closer.ps1
+++ d2r-handle-closer.ps1
@@ -1,18 +1,22 @@
 #Requires -RunAsAdministrator
 #Requires -PSEdition Core
 
-if (!(Test-Path handle.exe)) {
-    "handle.exe not found, make sure it is in the same folder as the script."
+if (!($env:Path -like "*$PSScriptRoot*")) {
+    $env:Path += ";$PSScriptRoot"
+}
+
+if (!(Get-Command handle -ErrorAction SilentlyContinue)) {
+    "handle.exe not found, make sure it is in path or the same folder as the script."
     exit 1
 }
 
 do {
 
-    $handle = .\handle.exe -a -p d2r "Check For Other Instances" -nobanner | Out-String
+    $handle = handle.exe -a -p d2r "Check For Other Instances" -nobanner | Out-String
     if ($handle -match "pid:\s+(?<d2pid>\d+)\s+type:\s+Event\s+(?<eventHandle>\w+):") {
         $d2pid = $matches["d2pid"]
         $eventHandle = $matches["eventHandle"]
-        .\handle.exe -c $eventHandle -p $d2pid -y -nobanner
+        handle.exe -c $eventHandle -p $d2pid -y -nobanner
 
         $exeIndex = (Get-Process -Id $d2pid).commandLine.IndexOf("exe")
         Get-Process D2R | Sort-Object -Property CommandLine | ForEach-Object { $_.CommandLine.Substring(0, ($_.CommandLine.Length -gt $exeIndex + 13) ? $exeIndex + 16 : $_.CommandLine.Length) }
